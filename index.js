c761afbf0e3f182c54a63d4d5d54be6bdde6c168
Wen Lim <zwlim@uwaterloo.ca>
2023-03-07 17:50:27 -0500

    added copyable prompts to images

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("javascript is working")
+const copyButtons = document.querySelectorAll(".copy-button");
+
+copyButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const text = button.parentNode.getAttribute("data-text");
+    navigator.clipboard.writeText(text);
+    alert(`"${text}" has been copied to the clipboard!`);
+  });
+});
