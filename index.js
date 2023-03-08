672cf665ef7c5355bb3d8f15f6c728a3525cb069
Wen Lim <zwlim@uwaterloo.ca>
2023-03-08 18:27:41 -0500

    forgotten commit. add history

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ copyButtons.forEach((button) => {
     alert(`"${text}" has been copied to the clipboard!`);
   });
 });
+
+const previousRequests = [
+  "Nature infused with AI",
+  "A beautiful mind",
+  "Duality of life",
+  "International Women's Day",
+  "A Dystopian City",
+  "Old War Veteran",
+];
+
+const datalist = document.querySelector("#previous-requests");
+
+previousRequests.forEach((request) => {
+  const option = document.createElement("option");
+  option.value = request;
+  datalist.appendChild(option);
+});
